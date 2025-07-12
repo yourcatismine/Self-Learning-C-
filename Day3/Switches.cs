00d6de71021ae0bb00e0492ca64a7817aea4894b
Kiara <kiaraiscuteee@gmail.com>
2025-07-13 00:37:05 +0800

    Changes Console Writeline LOL

--- Day3/Switches.cs
+++ Day3/Switches.cs
@@ -20,22 +20,22 @@ class Program
                 Console.WriteLine("Today is Monday!");
                 break;
             case "tuesday":
-                Console.WriteLine("Today is Monday!");
+                Console.WriteLine("Today is Tuesday!");
                 break;
             case "wednesday":
-                Console.WriteLine("Today is Monday!");
+                Console.WriteLine("Today is Wednesday!");
                 break;
             case "thursday":
-                Console.WriteLine("Today is Monday!");
+                Console.WriteLine("Today is Thursday!");
                 break;
             case "friday":
-                Console.WriteLine("Today is Monday!");
+                Console.WriteLine("Today is Friday!");
                 break;
             case "saturday":
-                Console.WriteLine("Today is Monday!");
+                Console.WriteLine("Today is Saturday!");
                 break;
             case "sunday":
-                Console.WriteLine("Today is Monday!");
+                Console.WriteLine("Today is Sunday!");
                 break;
             default: //Defualt is also like else
                 Console.WriteLine(DAY + " Is not a day!");
